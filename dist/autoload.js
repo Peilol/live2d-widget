f9e74cfcdd0106ba6f0845d748ef687f21b8cbe7
zh.Wang <a242723223@gmail.com>
2026-05-21 04:17:32 +0800

    fix: change @latest to @master for jsDelivr CDN

--- dist/autoload.js
+++ dist/autoload.js
@@ -6,7 +6,7 @@
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
 // const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.0-rc.6/dist/';
-const live2d_path = 'https://fastly.jsdelivr.net/gh/Peilol/live2d-widget@latest/dist/';
+const live2d_path = 'https://fastly.jsdelivr.net/gh/Peilol/live2d-widget@master/dist/';
 
 // Method to encapsulate asynchronous resource loading
 // 封装异步加载资源的方法
